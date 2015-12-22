e755c88f2708bb277759f41b45372eead8e6ef04
Seth Ladd <sethladd@google.com>
2015-12-21 17:32:13 -0800

    more

--- travis.sh
+++ travis.sh
@@ -5,11 +5,15 @@ if [ $TRAVIS_BRANCH = "master" ]; then
   npm install -g firebase-tools
   firebase deploy --token "$FIREBASE_TOKEN"
 
+  echo
   echo "Installing gcloud"
+  echo
 
   curl -o /tmp/google-cloud-sdk-91.0.1-linux-x86_64.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-91.0.1-linux-x86_64.tar.gz 
   tar xzf /tmp/google-cloud-sdk-91.0.1-linux-x86_64.tar.gz -C /tmp
   source '/tmp/google-cloud-sdk/path.bash.inc'
 
   which gcloud
+
+  gcloud auth activate-service-account --key-file sethladd-site-8958bc933793.json
 fi
