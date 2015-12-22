e32ce332934f24b8e739ba5098a5846c42f80b43
Seth Ladd <sethladd@google.com>
2015-12-21 17:16:10 -0800

    fix

--- travis.sh
+++ travis.sh
@@ -5,8 +5,10 @@ if [ $TRAVIS_BRANCH = "master" ]; then
   npm install -g firebase-tools
   firebase deploy --token "$FIREBASE_TOKEN"
 
+  echo "Installing gcloud"
+
   curl -o /tmp/google-cloud-sdk-91.0.1-linux-x86_64.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-91.0.1-linux-x86_64.tar.gz 
-  tar xzf /tmp/google-cloud-sdk-91.0.1-linux-x86_64.tar.gz /tmp/google-cloud-sdk
+  tar xzf /tmp/google-cloud-sdk-91.0.1-linux-x86_64.tar.gz /tmp
   source '/tmp/google-cloud-sdk/path.bash.inc'
 
   which gcloud
